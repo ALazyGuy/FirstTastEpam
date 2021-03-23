55c88f7522389e8c81ee737c83cc30e2070627bf
ALazyGuy <razorprogrammer@gmail.com>
2021-03-23 17:40:10 +0300

    feat: Created SearchService

---
+++ src/main/java/com/LTP/ArrayAPI/services/SearchService.java
@@ -0,0 +1,58 @@
+package com.LTP.ArrayAPI.services;
+
+import com.LTP.ArrayAPI.enities.ArrayEntity;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * SearchService
+ *
+ * SearchService allows to find minimal and maximal values in ArrayEntity
+ *
+ * @version 1.0.0 23 March 2021
+ * @author Daniil Selin
+ */
+
+public class SearchService {
+
+    private static final Logger LOGGER = LogManager.getLogger(SearchService.class);
+
+    /** Private constructor for singleton pattern */
+    private SearchService(){}
+
+    /**
+     * Find maximal value in input array {@link ArrayEntity}
+     *
+     * @see SearchService#findMinValue(ArrayEntity)
+     * @param arrayEntity input array
+     * @return Return maximal value in array
+     */
+    public static int findMaxValue(ArrayEntity arrayEntity){
+        int arr[] = arrayEntity.getArray();
+        int max = arr[0];
+        for(int d = 1; d < arr.length; d++){
+            if(max < arr[d]) max = arr[d];
+        }
+        LOGGER.log(Level.INFO, String.format("Max value in array %s is %d", arrayEntity.toString(), max));
+        return max;
+    }
+
+    /**
+     * Find minimal value in input array {@link ArrayEntity}
+     *
+     * @see SearchService#findMaxValue(ArrayEntity)
+     * @param arrayEntity input array
+     * @return Return minimal value in array
+     */
+    public static int findMinValue(ArrayEntity arrayEntity){
+        int arr[] = arrayEntity.getArray();
+        int min = arr[0];
+        for(int d = 1; d < arr.length; d++){
+            if(min > arr[d]) min = arr[d];
+        }
+        LOGGER.log(Level.INFO, String.format("Min value in array %s is %d", arrayEntity.toString(), min));
+        return min;
+    }
+
+}
--- src/main/java/com/LTP/Start.java
+++ src/main/java/com/LTP/Start.java
@@ -1,17 +1,14 @@
 package com.LTP;
 
 import com.LTP.ArrayAPI.enities.ArrayEntity;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import com.LTP.ArrayAPI.services.SearchService;
 
 public class Start {
 
-    private static final Logger logger = LogManager.getLogger(Start.class);
-
     public static void main(String[] args) {
         ArrayEntity entity = new ArrayEntity(new int[]{1, 7, -4, 7, 2});
-        logger.log(Level.DEBUG, entity.toString());
+        SearchService.findMaxValue(entity);
+        SearchService.findMinValue(entity);
     }
 
 }
