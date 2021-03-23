9300bbd3341cb2c9c1a96f2a516a1c7907d26882
ALazyGuy <razorprogrammer@gmail.com>
2021-03-23 17:59:05 +0300

    feat: Added test for SearchService

---
+++ src/test/java/test/com/LTP/ArrayAPI/services/SearchServiceTest.java
@@ -0,0 +1,47 @@
+package test.com.LTP.ArrayAPI.services;
+
+import com.LTP.ArrayAPI.enities.ArrayEntity;
+import com.LTP.ArrayAPI.services.SearchService;
+import org.testng.AssertJUnit;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class SearchServiceTest {
+
+    private ArrayEntity arrayEntity;
+
+    @BeforeClass
+    public void init(){
+        arrayEntity = new ArrayEntity(new int[]{5, 1, -6, 0, 45, 0, -19, 3});
+    }
+
+    @Test
+    public void findMaxValueTest(){
+        int expected = 45;
+        int result = SearchService.findMaxValue(arrayEntity);
+        AssertJUnit.assertEquals(expected, result);
+    }
+
+    @Test
+    public void findMinValueTest(){
+        int expected = -19;
+        int result = SearchService.findMinValue(arrayEntity);
+        AssertJUnit.assertEquals(expected, result);
+    }
+
+    @Test(expectedExceptions = {IllegalArgumentException.class})
+    public void findMaxValueExceptionTest(){
+        SearchService.findMaxValue(null);
+    }
+
+    @Test(expectedExceptions = {IllegalArgumentException.class})
+    public void findMinValueExceptionTest(){
+        SearchService.findMinValue(null);
+    }
+
+    @AfterClass
+    public void terminate(){
+        arrayEntity = null;
+    }
+}
