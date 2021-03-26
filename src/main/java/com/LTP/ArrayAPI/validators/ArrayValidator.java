a1d40ba5930ee85c123ca3f1ac1a278c05eeaa08
ALazyGuy <razorprogrammer@gmail.com>
2021-03-26 13:07:43 +0300

    fix: Changed validation method

--- src/main/java/com/LTP/ArrayAPI/validators/ArrayValidator.java
+++ src/main/java/com/LTP/ArrayAPI/validators/ArrayValidator.java
@@ -1,6 +1,7 @@
 package com.LTP.ArrayAPI.validators;
 
 import com.LTP.ArrayAPI.enities.ArrayEntity;
+import com.LTP.ArrayAPI.exceptions.ArrayException;
 
 public class ArrayValidator {
 
@@ -10,10 +11,11 @@ public class ArrayValidator {
     /**
      * validateArray allows you to validate array(not empty, not null)
      * @param arrayEntity - input array
-     * @return is array valid
+     * @throws ArrayException will be thrown if array is invalid
      */
-    public static boolean validateArray(ArrayEntity arrayEntity){
-        return (arrayEntity != null) && (arrayEntity.getArray().length > 0);
+    public static void validateArray(ArrayEntity arrayEntity) throws ArrayException{
+        if(arrayEntity == null) throw new ArrayException("Input array cannot be null");
+        if(arrayEntity.getArray().length == 0) throw new ArrayException("Input array cannot be empty");
     }
 
 }
