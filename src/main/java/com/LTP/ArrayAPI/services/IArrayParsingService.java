08eae564d9fcd0db49b2214e06d7ec587ce2e509
ALazyGuy <razorprogrammer@gmail.com>
2021-03-27 13:27:40 +0300

    feat: Added array parsing service

---
+++ src/main/java/com/LTP/ArrayAPI/services/IArrayParsingService.java
@@ -0,0 +1,7 @@
+package com.LTP.ArrayAPI.services;
+
+import com.LTP.ArrayAPI.enities.ArrayEntity;
+
+public interface IArrayParsingService {
+    ArrayEntity parse(String line);
+}
---
+++ src/main/java/com/LTP/ArrayAPI/services/impl/ArrayParsingService.java
@@ -0,0 +1,51 @@
+package com.LTP.ArrayAPI.services.impl;
+
+import com.LTP.ArrayAPI.enities.ArrayEntity;
+import com.LTP.ArrayAPI.services.IArrayParsingService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+/**
+ * ArrayParsingService
+ *
+ * ArrayParsingService allows to parse String to {@link ArrayEntity}
+ *
+ * @version 1.0.0 27 March 2021
+ * @author Daniil Selin
+ */
+public class ArrayParsingService implements IArrayParsingService {
+
+    private static final Logger LOGGER = LogManager.getLogger(ArrayParsingService.class);
+    private static ArrayParsingService instance;
+
+    /** Private constructor for singleton pattern */
+    private ArrayParsingService(){}
+
+    /**
+     * getInstance method allows to get class instance in singleton pattern
+     * @return static instance of current class
+     */
+    public static ArrayParsingService getInstance(){
+        if(instance == null){
+            instance = new ArrayParsingService();
+        }
+
+        return instance;
+    }
+
+    /**
+     * parse method allows you to parse input String to {@link ArrayEntity}
+     * @param line - input String
+     * @return ArrayEntity
+     */
+    @Override
+    public ArrayEntity parse(String line) {
+        String changed = Pattern.compile("(\\s+\\-\\s+|,\\s+)").matcher(line).replaceAll(" ");
+        String[] numbers = changed.split(" ", 0);
+        int[] array = Arrays.stream(numbers).mapToInt(Integer::parseInt).toArray();
+        return new ArrayEntity(array);
+    }
+}
