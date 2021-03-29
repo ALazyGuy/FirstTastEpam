56b341d74efb263f54e2fb1fc6d481ea890f5e19
ALazyGuy <razorprogrammer@gmail.com>
2021-03-29 09:23:07 +0300

    feat: Added stream methods for Calculating Service

--- src/main/java/com/LTP/ArrayAPI/services/ICalculateService.java
+++ src/main/java/com/LTP/ArrayAPI/services/ICalculateService.java
@@ -5,8 +5,13 @@ import com.LTP.ArrayAPI.exceptions.ArrayException;
 
 public interface ICalculateService {
     double average(ArrayEntity arrayEntity) throws ArrayException;
+    double averageStream(ArrayEntity arrayEntity) throws ArrayException;
     int sum(ArrayEntity arrayEntity) throws ArrayException;
+    int sumStream(ArrayEntity arrayEntity) throws ArrayException;
     int sum(ArrayEntity arrayEntity, int start, int stop) throws ArrayException;
+    int sumStream(ArrayEntity arrayEntity, int start, int stop) throws ArrayException;
     int countPositives(ArrayEntity arrayEntity) throws ArrayException;
+    int countPositivesStream(ArrayEntity arrayEntity) throws ArrayException;
     int countNegatives(ArrayEntity arrayEntity) throws ArrayException;
+    int countNegativesStream(ArrayEntity arrayEntity) throws ArrayException;
 }
--- src/main/java/com/LTP/ArrayAPI/services/impl/CalculateServiceImpl.java
+++ src/main/java/com/LTP/ArrayAPI/services/impl/CalculateServiceImpl.java
@@ -36,38 +36,38 @@ public class CalculateServiceImpl implements ICalculateService {
     }
 
     /**
-     * average method allows you to find average of an array
+     * averageStream method allows you to find average of an array
      * @param arrayEntity - input array
      * @return return average of an input array
      */
     @Override
-    public double average(ArrayEntity arrayEntity) throws ArrayException {
+    public double averageStream(ArrayEntity arrayEntity) throws ArrayException {
         ArrayValidator.validateArray(arrayEntity);
-        double result = Arrays.stream(arrayEntity.getArray()).sum();
-        LOGGER.log(Level.INFO, String.format("Average of an %s is %f", arrayEntity.toString(), result / arrayEntity.getArray().length));
-        return result / arrayEntity.getArray().length;
+        double average = Arrays.stream(arrayEntity.getArray()).average().getAsDouble();
+        LOGGER.log(Level.INFO, String.format("Average of an %s is %f", arrayEntity.toString(), average));
+        return average;
     }
 
     /**
-     * sum(ArrayEntity) method allows you to find sum of all elements in array
+     * sumStream(ArrayEntity) method allows you to find sum of all elements in array
      * @param arrayEntity - input array
      * @return return sum of all elements in input array
-     * @see CalculateServiceImpl#sum(ArrayEntity, int, int)
+     * @see CalculateServiceImpl#sumStream(ArrayEntity, int, int)
      */
     @Override
-    public int sum(ArrayEntity arrayEntity) throws ArrayException{
+    public int sumStream(ArrayEntity arrayEntity) throws ArrayException{
         ArrayValidator.validateArray(arrayEntity);
-        return sum(arrayEntity, 0, arrayEntity.getArray().length - 1);
+        return sumStream(arrayEntity, 0, arrayEntity.getArray().length - 1);
     }
 
     /**
-     * sum(ArrayEntity, int, int) method allows you to find sum of elements between start and stop indexes including them
+     * sumStream(ArrayEntity, int, int) method allows you to find sum of elements between start and stop indexes including them
      * @param arrayEntity - input array
      * @return return sum of elements between start and stop indexes including them
-     * @see CalculateServiceImpl#sum(ArrayEntity)
+     * @see CalculateServiceImpl#sumStream(ArrayEntity)
      */
     @Override
-    public int sum(ArrayEntity arrayEntity, int start, int stop) throws ArrayException{
+    public int sumStream(ArrayEntity arrayEntity, int start, int stop) throws ArrayException{
         ArrayValidator.validateArray(arrayEntity);
         if(start < 0 || start >= stop || stop >= arrayEntity.getArray().length) throw new ArrayException("Invalid bounds in input array");
         int result = Arrays.stream(arrayEntity.getArray()).skip(start).limit(stop - start + 1).sum();
@@ -80,16 +80,106 @@ public class CalculateServiceImpl implements ICalculateService {
     }
 
     /**
+     * countPositivesStream method allows you to count all positive number in an array
+     * @param arrayEntity - input array
+     * @return return count of all positive numbers including zero
+     * @see CalculateServiceImpl#countNegatives(ArrayEntity)
+     */
+    @Override
+    public int countPositivesStream(ArrayEntity arrayEntity) throws ArrayException{
+        ArrayValidator.validateArray(arrayEntity);
+        int result = (int)Arrays.stream(arrayEntity.getArray()).filter(a -> a >= 0).count();
+        LOGGER.log(Level.INFO, String.format("Count of positive numbers in %s is %d", arrayEntity.toString(), result));
+        return result;
+    }
+
+    /**
+     * countNegativesStream method allows you to count all negative number in an array
+     * @param arrayEntity - input array
+     * @return return count of all negative numbers except zero
+     * @see CalculateServiceImpl#countPositives(ArrayEntity)
+     */
+    @Override
+    public int countNegativesStream(ArrayEntity arrayEntity) throws ArrayException{
+        ArrayValidator.validateArray(arrayEntity);
+        int result = arrayEntity.getArray().length - countPositivesStream(arrayEntity);
+        LOGGER.log(Level.INFO, String.format("Count of negative numbers in %s is %d", arrayEntity.toString(), result));
+        return result;
+    }
+
+    /**
+     * average method allows you to find average of an array
+     * @param arrayEntity - input array
+     * @return return average of an input array
+     */
+    @Override
+    public double average(ArrayEntity arrayEntity) throws ArrayException {
+        ArrayValidator.validateArray(arrayEntity);
+        int[] array = arrayEntity.getArray();
+        double average = sum(arrayEntity);
+        average /= array.length;
+        LOGGER.log(Level.INFO, String.format("Average of an %s is %f", arrayEntity.toString(), average));
+        return average;
+    }
+
+    /**
+     * sum(ArrayEntity) method allows you to find sum of all elements in array
+     * @param arrayEntity - input array
+     * @return return sum of all elements in input array
+     * @see CalculateServiceImpl#sum(ArrayEntity, int, int)
+     */
+    @Override
+    public int sum(ArrayEntity arrayEntity) throws ArrayException {
+        ArrayValidator.validateArray(arrayEntity);
+        return sumStream(arrayEntity, 0, arrayEntity.getArray().length - 1);
+    }
+
+    /**
+     * sum(ArrayEntity, int, int) method allows you to find sum of elements between start and stop indexes including them
+     * @param arrayEntity - input array
+     * @return return sum of elements between start and stop indexes including them
+     * @see CalculateServiceImpl#sum(ArrayEntity)
+     */
+    @Override
+    public int sum(ArrayEntity arrayEntity, int start, int stop) throws ArrayException {
+        ArrayValidator.validateArray(arrayEntity);
+        if(start < 0 || start >= stop || stop >= arrayEntity.getArray().length) throw new ArrayException("Invalid bounds in input array");
+
+        int[] array = arrayEntity.getArray();
+        int result = 0;
+
+        for(int current : array){
+            result += current;
+        }
+
+        LOGGER.log(Level.INFO, String.format("Sum of %s from index %d to index %d is %d",
+                arrayEntity.toString(),
+                start,
+                stop,
+                result));
+
+        return result;
+    }
+
+    /**
      * countPositives method allows you to count all positive number in an array
      * @param arrayEntity - input array
      * @return return count of all positive numbers including zero
      * @see CalculateServiceImpl#countNegatives(ArrayEntity)
      */
     @Override
-    public int countPositives(ArrayEntity arrayEntity) throws ArrayException{
+    public int countPositives(ArrayEntity arrayEntity) throws ArrayException {
         ArrayValidator.validateArray(arrayEntity);
-        int result = (int)Arrays.stream(arrayEntity.getArray()).filter(a -> a >= 0).count();
+
+        int result = 0;
+        int[] array = arrayEntity.getArray();
+
+        for(int current : array){
+            if(current >= 0) result++;
+        }
+
         LOGGER.log(Level.INFO, String.format("Count of positive numbers in %s is %d", arrayEntity.toString(), result));
+
         return result;
     }
 
@@ -100,11 +190,10 @@ public class CalculateServiceImpl implements ICalculateService {
      * @see CalculateServiceImpl#countPositives(ArrayEntity)
      */
     @Override
-    public int countNegatives(ArrayEntity arrayEntity) throws ArrayException{
+    public int countNegatives(ArrayEntity arrayEntity) throws ArrayException {
         ArrayValidator.validateArray(arrayEntity);
         int result = arrayEntity.getArray().length - countPositives(arrayEntity);
         LOGGER.log(Level.INFO, String.format("Count of negative numbers in %s is %d", arrayEntity.toString(), result));
         return result;
     }
-
 }
